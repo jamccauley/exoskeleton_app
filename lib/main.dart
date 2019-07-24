d0f58905f8df0b6d6370c56532f280718d342888
jamccauley <jm9696@gmail.com>
2019-07-23 20:52:10 -0400

    changed the top widget to app bar and drawer

--- lib/main.dart
+++ lib/main.dart
@@ -15,26 +15,16 @@ class HomeState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+
+      ),
+      drawer: Drawer(
+
+      ),
       backgroundColor: Colors.blue,
       body: SingleChildScrollView( //define the scrollview, may need to switch to a list view at some point to enable reloading of new exercises
         child: Column( //define the main column
           children: <Widget>[ //not sure why this widget is here, maybe to make all children of the scroll view interactive??
-            Padding( //padding to offset edges of buttons
-                padding: const EdgeInsets.only( //define padding, .only gives full control of all directions, .all sets all pads the same
-                  left: 12, right: 12, top: 30,bottom: 8),
-                child: Row( //first row in the scroll view
-                  children: <Widget>[ //first item in the row is a widget (menu button)
-                    IconButton( //defining the button using the icon button class
-                      icon: Icon(
-                        IconData(58834, fontFamily: 'MaterialIcons'),
-                        color: Colors.white,
-                        size: 60,
-                      ),
-                    onPressed: () {}, //on pressed method for raising the menu
-                  )
-                ],
-              )
-            ),
             Padding( //padding for second row
               padding: const EdgeInsets.all(12), //using .all ...
               child: Row( //second row
@@ -60,6 +50,7 @@ class HomeState extends State<Homepage> {
                       textColor: Colors.blue,
                       color: Colors.white,
                       padding: EdgeInsets.all(12),
+
                       child: Text( //defining the text within the button
                         "Exercise 2",
                         style: TextStyle(fontSize: 32),
@@ -75,4 +66,5 @@ class HomeState extends State<Homepage> {
     );
   }
 }
+
 
